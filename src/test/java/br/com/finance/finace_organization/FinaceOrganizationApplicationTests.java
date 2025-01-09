79aa9fea50b29103620842c4320982036b7a8dc0
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2025-01-08 21:29:56 -0300

    first commit

---
+++ src/test/java/br/com/finance/finace_organization/FinaceOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finance.finace_organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaceOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
